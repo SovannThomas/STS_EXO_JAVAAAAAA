e840e2ef56e4be70f0fba0104a8a95c54a423e32
SovannThomas <thomasovann@gmail.com>
2023-12-10 01:07:24 +0100

    init mais en java cette fois

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "STS_EXO_HT_LT_JAVA"
+include(":app")
+ 
