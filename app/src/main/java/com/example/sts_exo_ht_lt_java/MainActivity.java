c42020bf096df7c77ff4d9db33d9d90d6a4249cf
SovannThomas <thomasovann@gmail.com>
2023-12-10 01:35:04 +0100

    Réalisation Front

--- app/src/main/java/com/example/sts_exo_ht_lt_java/MainActivity.java
+++ app/src/main/java/com/example/sts_exo_ht_lt_java/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.sts_exo_ht_lt_java;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvMat;
+    private Model model = new Model();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvMat = findViewById(R.id.listMatView);
+        ModelAdapter adapter = new ModelAdapter(this);
+        adapter.setModel(model);
+        lvMat.setAdapter(adapter);
     }
 }
